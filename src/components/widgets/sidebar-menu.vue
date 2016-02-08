a2fbfdbe705727b58799fc202bef688261f12f34
Tyler Mulligan <tmulligan@sans.org>
2016-02-08 16:40:16 -0500

    updated menu structure

--- src/components/widgets/sidebar-menu.vue
+++ src/components/widgets/sidebar-menu.vue
@@ -2,21 +2,29 @@
   <!-- sidebar menu: : style can be found in sidebar.less -->
   <ul class="sidebar-menu">
     <li class="header">MAIN NAVIGATION</li>
-    <li class="treeview" v-for="item in menu">
-      <a v-link="{ path: item.link }">
-        <i class="fa {{ item.icon }}"></i>
-        <span>{{ item.name }}</span>
-        <i v-show="item.child.length" class="fa fa-angle-left pull-right"></i>
-      </a>
+    <template v-for="item in menu">
+      <li v-if="item.child" class="treeview" v-link-active>
+        <a v-link="{ path: item.link }">
+          <i class="fa {{ item.icon }}"></i>
+          <span>{{ item.name }}</span>
+          <i v-if="item.child" class="fa fa-angle-left pull-right"></i>
+        </a>
 
-      <ul v-show="item.child.length" class="treeview-menu">
-        <li v-link-active v-for="child in item.child">
-          <a v-link="{ path: child.link }">
-            <i class="fa {{ child.icon }}"></i> {{ child.name }}
-          </a>
-        </li>
-      </ul>
-    </li>
+        <ul class="treeview-menu">
+          <li v-link-active v-for="child in item.child">
+            <a v-link="{ path: child.link }">
+              <i class="fa {{ child.icon }}"></i> {{ child.name }}
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li v-else v-link-active>
+        <a v-link="{ path: item.link, exact: true }">
+          <i class="fa {{ item.icon }}"></i>
+          <span>{{ item.name }}</span>
+        </a>
+      </li>
+    </template>
   </ul>
 </template>
 
