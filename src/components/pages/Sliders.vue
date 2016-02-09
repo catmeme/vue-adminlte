48a8520627278eae437c6bfc2434cbda25bf2b4e
Tyler Mulligan <tmulligan@sans.org>
2016-02-09 16:13:28 -0500

    remove that because it's broken right now

--- src/components/pages/Sliders.vue
+++ src/components/pages/Sliders.vue
@@ -72,73 +72,71 @@
 
 <script>
   // This doesn't work yet, I'm not sure why
-  import $ from 'jquery'
-  import '../../../static/plugins/bootstrap-slider/bootstrap-slider.js'
-  import '../../../static/plugins/ionslider/ion.rangeSlider.min.js'
+  module.exports = {
+    ready: function ($) {
 
-  $(function () {
+      /* BOOTSTRAP SLIDER */
+      $('.slider').slider()
 
-    /* BOOTSTRAP SLIDER */
-    $('.slider').slider()
+      /* ION SLIDER */
+      $('#range_1').ionRangeSlider({
+        min: 0,
+        max: 5000,
+        from: 1000,
+        to: 4000,
+        type: 'double',
+        step: 1,
+        prefix: '$',
+        prettify: false,
+        hasGrid: true
+      })
+      $('#range_2').ionRangeSlider()
 
-    /* ION SLIDER */
-    $('#range_1').ionRangeSlider({
-      min: 0,
-      max: 5000,
-      from: 1000,
-      to: 4000,
-      type: 'double',
-      step: 1,
-      prefix: '$',
-      prettify: false,
-      hasGrid: true
-    })
-    $('#range_2').ionRangeSlider()
+      $('#range_5').ionRangeSlider({
+        min: 0,
+        max: 10,
+        type: 'single',
+        step: 0.1,
+        postfix: ' mm',
+        prettify: false,
+        hasGrid: true
+      })
+      $('#range_6').ionRangeSlider({
+        min: -50,
+        max: 50,
+        from: 0,
+        type: 'single',
+        step: 1,
+        postfix: '°',
+        prettify: false,
+        hasGrid: true
+      })
 
-    $('#range_5').ionRangeSlider({
-      min: 0,
-      max: 10,
-      type: 'single',
-      step: 0.1,
-      postfix: ' mm',
-      prettify: false,
-      hasGrid: true
-    })
-    $('#range_6').ionRangeSlider({
-      min: -50,
-      max: 50,
-      from: 0,
-      type: 'single',
-      step: 1,
-      postfix: '°',
-      prettify: false,
-      hasGrid: true
-    })
-
-    $('#range_4').ionRangeSlider({
-      type: 'single',
-      step: 100,
-      postfix: ' light years',
-      from: 55000,
-      hideMinMax: true,
-      hideFromTo: false
-    })
-    $('#range_3').ionRangeSlider({
-      type: 'double',
-      postfix: ' miles',
-      step: 10000,
-      from: 25000000,
-      to: 35000000,
-      onChange: function (obj) {
-        var t = ''
-        for (var prop in obj) {
-          t += prop + ': ' + obj[prop] + '\r\n'
+      $('#range_4').ionRangeSlider({
+        type: 'single',
+        step: 100,
+        postfix: ' light years',
+        from: 55000,
+        hideMinMax: true,
+        hideFromTo: false
+      })
+      $('#range_3').ionRangeSlider({
+        type: 'double',
+        postfix: ' miles',
+        step: 10000,
+        from: 25000000,
+        to: 35000000,
+        onChange: function (obj) {
+          var t = ''
+          for (var prop in obj) {
+            t += prop + ': ' + obj[prop] + '\r\n'
+          }
+          $('#result').html(t)
+        },
+        onLoad: function (obj) {
+          //
         }
-        $('#result').html(t)
-      },
-      onLoad: function (obj) {
-        //
-      }
-    })
-  })
+      })
+    }
+  }
 </script>
